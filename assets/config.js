a0f281d4cdd50a0e753c6a4e5bcf6ac434ebf539
sdcomayagua <sdcomayagua@gmail.com>
2026-02-08 13:29:09 -0600

    Add files via upload

--- assets/config.js
+++ assets/config.js
@@ -7,7 +7,7 @@ window.SDCO_DEFAULTS = {
   "SHORT_NAME": "SDComayagua",
   "WHATSAPP_DISPLAY": "+504 3151-7755",
   "WHATSAPP_NUMBER": "50431517755",
-  "API_BASE_DEFAULT": "https://script.google.com/macros/s/AKfycbytPfD9mq__VO7I2lnpBsqdCIT119ZT0zVyz0eeVjrJVgN_q8FYGgmqY6G66C2m67Pa4g/exec",
+  "API_BASE_DEFAULT": "https://script.google.com/macros/s/AKfycbya37aSm80xgzd7mh4mG87_gRZzvl55xl4gt3X5hyCPvUeDg6chLJq7Qn97n_aqS3nI/exec",
   "CURRENCY": "Lps.",
   "LOCALE": "es-HN"
 };
