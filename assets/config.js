16c0196212221b5a472246cedf33904e0c31ab62
sdcomayagua <sdcomayagua@gmail.com>
2026-02-08 13:23:55 -0600

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,13 @@
+/**
+ * Configuración (NO hace falta editar código)
+ * Puedes cambiar esto desde Admin → Configuración (se guarda en este navegador).
+ */
+window.SDCO_DEFAULTS = {
+  "APP_NAME": "Soluciones Digitales Comayagua",
+  "SHORT_NAME": "SDComayagua",
+  "WHATSAPP_DISPLAY": "+504 3151-7755",
+  "WHATSAPP_NUMBER": "50431517755",
+  "API_BASE_DEFAULT": "https://script.google.com/macros/s/AKfycbytPfD9mq__VO7I2lnpBsqdCIT119ZT0zVyz0eeVjrJVgN_q8FYGgmqY6G66C2m67Pa4g/exec",
+  "CURRENCY": "Lps.",
+  "LOCALE": "es-HN"
+};
